0c2f93c930fae1b898ba1ccc4b78fdb353c0cec7
unknown <mindsdet130@iiht.tech>
2022-06-08 12:11:12 +0530

    Adding project

---
+++ src/test/java/jenkinsproject/Demo.java
@@ -0,0 +1,14 @@
+package jenkinsproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Opening Google");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mindsdet130\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+}
